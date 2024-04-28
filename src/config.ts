8d1a23364020fe9d1358337aa9c092de295ea877
wuhunfeng <wuhunfeng@163.com>
2024-04-28 15:24:07 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -71,7 +71,7 @@ const GLOBAL_CONFIG = {
   /**
    * 背景图片地址
    */
-  BACKGROUND_IMG_URL: "https://api.dujin.org/bing/1921.php",
+  BACKGROUND_IMG_URL: "https://api.dujin.org/bing/1920.php",
   /**
    * ICP 备案号，留空不显示
    */
